76c2935bca3fca1025e2578d3ac3b3f2471bdd25
ys57mhn2b6-png <ys57mhn2b6@privaterelay.appleid.com>
2026-07-19 15:50:57 +1200

    updated create session

--- supabase/functions/create-portal-session/index.ts
+++ supabase/functions/create-portal-session/index.ts
@@ -17,7 +17,7 @@
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-import Stripe from "npm:stripe@14.21.0?target=deno";
+import Stripe from "npm:stripe@14.21.0";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
